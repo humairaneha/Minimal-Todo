ddebd2c83203c3d4230533ac655170062ed47e21
humairaneha <humairaanzumn@gmail.com>
2022-05-17 22:31:36 +0600

    new file containing a java class is added

---
+++ app/src/androidTest/java/Main/File.java
@@ -0,0 +1,30 @@
+package Main;
+
+public class File {
+
+    public:
+       int i;
+    int j;
+
+    public File(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+}
+

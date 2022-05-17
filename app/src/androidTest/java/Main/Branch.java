fb165830ec46d80ce270642dbe649e0f968b15b4
humairaneha <humairaanzumn@gmail.com>
2022-05-18 00:18:15 +0600

    modified the java class in branch2 that was previously created in branch1

--- app/src/androidTest/java/Main/Branch.java
+++ app/src/androidTest/java/Main/Branch.java
@@ -2,17 +2,10 @@ package Main;
 
 public class Branch {
    public:
-    String branch1;
+    String branch2;
 
     public Branch(String branch1) {
-        this.branch1 = branch1;
+        this.branch2 = branch1;
     }
-
-    public String getBranch1() {
-        return branch1;
-    }
-
-    public void setBranch1(String branch1) {
-        this.branch1 = branch1;
-    }
+    
 }

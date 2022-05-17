d1e749ba715014e79cafe34835b454c983599432
humairaneha <humairaanzumn@gmail.com>
2022-05-18 00:07:13 +0600

    added a file with a java class in branch1

---
+++ app/src/androidTest/java/Main/Branch.java
@@ -0,0 +1,18 @@
+package Main;
+
+public class Branch {
+   public:
+    String branch1;
+
+    public Branch(String branch1) {
+        this.branch1 = branch1;
+    }
+
+    public String getBranch1() {
+        return branch1;
+    }
+
+    public void setBranch1(String branch1) {
+        this.branch1 = branch1;
+    }
+}
